e9ad66af028d00268991c0abe2248dabfcfdbddc
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-11-24 16:28:11 +0900

    add selenium

--- main.go
+++ main.go
@@ -4,14 +4,160 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/tebeka/selenium"
+	"github.com/tebeka/selenium/chrome"
 )
 
 var baseURL string = "https://pcmap.place.naver.com/place/list?query=%ED%95%9C%EC%8B%9D%EB%B7%94%ED%8E%98&x=127.59770646143932&y=35.845355799999766&clientX=126.942428&clientY=37.485309&bounds=125.88385325590485%3B33.001391604349834%3B129.39435450781872%3B38.526144869437104&ts=1700727250844&mapUrl=https%3A%2F%2Fmap.naver.com%2Fp%2Fsearch%2F%ED%95%9C%EC%8B%9D%EB%B7%94%ED%8E%98"
 
+// define a custom data type for the scraped data
+type Product struct {
+	name, price string
+}
+
 func main() {
-	getTest()
+	//getTest()
+	//sel()
+	test()
+}
+
+func test() {
+	// initialize a Chrome browser instance on port 4444
+	service, err := selenium.NewChromeDriverService("./chromedriver", 4444)
+
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	defer service.Stop()
+
+	// configure the browser options
+
+	caps := selenium.Capabilities{}
+	caps.AddChrome(chrome.Capabilities{Args: []string{
+		"--headless", // comment out this line for testing
+	}})
+
+	// create a new remote client with the specified options
+	driver, err := selenium.NewRemote(caps, "")
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	// visit the target page
+	err = driver.Get("https://pcmap.place.naver.com/place/list?query=%ED%95%9C%EC%8B%9D%EB%B7%94%ED%8E%98&x=127.49842900465427&y=35.66375922262287&clientX=126.942428&clientY=37.485309&bounds=125.38429339311597%3B32.13406719963322%3B129.7146967713403%3B38.96469205760175&ts=1700809713850&mapUrl=https%3A%2F%2Fmap.naver.com%2Fp%2Fsearch%2F%ED%95%9C%EC%8B%9D%EB%B7%94%ED%8E%98/")
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	// retrieve the page raw HTML as a string
+	// and logging it
+
+	html, err := driver.PageSource()
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	fmt.Println(html)
+}
+
+func sel() {
+	// where to store the scraped data
+	var products []Product
+
+	// initialize a Chrome browser instance on port 4444
+	service, err := selenium.NewChromeDriverService("./chromedriver", 4444)
+
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	defer service.Stop()
+
+	// configure the browser options
+
+	caps := selenium.Capabilities{}
+	caps.AddChrome(chrome.Capabilities{Args: []string{
+		"--headless", // comment out this line for testing
+	}})
+
+	// create a new remote client with the specified options
+	driver, err := selenium.NewRemote(caps, "")
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	// visit the target page
+	err = driver.Get("https://pcmap.place.naver.com/place/list?query=%ED%95%9C%EC%8B%9D%EB%B7%94%ED%8E%98&x=127.49842900465427&y=35.66375922262287&clientX=126.942428&clientY=37.485309&bounds=125.38429339311597%3B32.13406719963322%3B129.7146967713403%3B38.96469205760175&ts=1700809713850&mapUrl=https%3A%2F%2Fmap.naver.com%2Fp%2Fsearch%2F%ED%95%9C%EC%8B%9D%EB%B7%94%ED%8E%98")
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	// perform the scrolling interaction
+	scrollingScript := `
+
+	 // scroll down the page 10 times
+	 const scrolls = 10
+	 let scrollCount = 0
+   
+   // scroll down and then wait for 0.5s
+	 const scrollInterval = setInterval(() => {
+	  window.scrollTo(0, document.body.scrollHeight)
+	  scrollCount++
+   
+	  if (scrollCount === scrolls) {
+	  clearInterval(scrollInterval)
+	  }
+	 }, 500)
+	 `
+	_, err = driver.ExecuteScript(scrollingScript, []interface{}{})
+
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	// wait up to 10 seconds for the 60th product to be on the page
+	err = driver.WaitWithTimeout(func(driver selenium.WebDriver) (bool, error) {
+		lastProduct, _ := driver.FindElement(selenium.ByCSSSelector, ".UEzoS rTjJo cZnHG")
+		if lastProduct != nil {
+			return lastProduct.IsDisplayed()
+		}
+		return false, nil
+	}, 20*time.Second)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	// select the product elements
+	productElements, err := driver.FindElements(selenium.ByCSSSelector, ".UEzoS rTjJo cZnHG")
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	// iterate over the product elements
+	// and extract data from them
+	for _, productElement := range productElements {
+		// select the name and price nodes
+		nameElement, err := productElement.FindElement(selenium.ByCSSSelector, ".TYaxT")
+		priceElement, err := productElement.FindElement(selenium.ByCSSSelector, ".Pb4bU")
+
+		// extract the data of interest
+		name, err := nameElement.Text()
+		price, err := priceElement.Text()
+		if err != nil {
+			log.Fatal("Error:", err)
+		}
+
+		// add the scraped data to the list
+		product := Product{}
+		product.name = name
+		product.price = price
+		products = append(products, product)
+	}
+
+	fmt.Println(products)
+
 }
 
 func getTest() int {
